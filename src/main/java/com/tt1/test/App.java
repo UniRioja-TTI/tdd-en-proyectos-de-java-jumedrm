0bf14b2f60482da3aeff76c13389e494b00150ce
jumedrm <jumedrm@CCTL138-12.unirioja.loc>
2026-02-23 16:05:30 +0100

    Primer commit, con el main de App.java con contenido.

---
+++ src/main/java/com/tt1/test/App.java
@@ -0,0 +1,11 @@
+package com.tt1.test;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hola mundo desde el proyecto de tdd");
+    }
+}
